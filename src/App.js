bf71fb2a2e18dd7d8cab0880ccc9f4249911e79a
kobesparrow <markcollins.md@gmail.com>
2019-05-16 10:39:43 -0600

    Delete Reservations operational

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ class App extends Component {
   }
 
   componentDidMount() {
-    // this.fetchUpdatedReservations();
     fetch('http://localhost:3001/api/v1/reservations')
       .then(response => response.json())
       .then(result => this.setState({ reservations: [...result] }))
@@ -22,6 +21,7 @@ class App extends Component {
 
   makeReservation = (reservation) => {
     const fullReservation = {...reservation, number: this.state.reservations.length + 1}
+    this.setState({ reservations: [...this.state.reservations, fullReservation] })
     console.log(fullReservation)
     fetch('http://localhost:3001/api/v1/reservations', {
       method: 'POST',
@@ -31,7 +31,7 @@ class App extends Component {
       }
     })
       .catch(error => console.log(error.message))
-    this.fetchUpdatedReservations() 
+    this.handleUpdate() 
   }
 
   fetchUpdatedReservations = () => {
@@ -43,7 +43,10 @@ class App extends Component {
   }
 
   deleteReservation = (id) => {
-    console.log();
+    const reservations = this.state.reservations.filter(reservation => {
+      return reservation.id !== id
+    })
+    this.setState({ reservations })
     fetch(`http://localhost:3001/api/v1/reservations/${id}`, {
       method: 'DELETE',
       headers: {
